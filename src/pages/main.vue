7d1cd43c450f244c7dbe5fcf7e75a2563dbde4d8
AquirJan <aquirjan@AquirJans-MacBook.local>
2017-05-04 11:06:43 +0800

    normal commit

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -3,11 +3,15 @@
 		<div class="t-style cntr-ml">
 			{{msg}}
 		</div>
+		<button class="btn btn-info" v-on:click="testApi('PUT')">put</button>
+		<button class="btn btn-info" v-on:click="testApi('POST')">post</button>
+		<button class="btn btn-info" v-on:click="testApi('DELETE')">delete</button>
     </div>
 </template>
 
 <script>
 // 	import moment from 'moment';
+	import xhres6 from '../public/js/xhres6.js';
     export default {
     	components:{
     	},
@@ -26,6 +30,15 @@
 
     	},
     	methods:{
+    		testApi(_method){
+    			const xhr = new xhres6({
+    				method:_method,
+					url:'https://api.dokodm.com/',
+    			});
+    			xhr.then((_rpdata)=>{
+    				console.log(_rpdata);
+    			})
+    		}
     		
     	},
     }
